3e0bbc4eda1973df521e1bf2f7d3a4a12bfb6168
Rakesh Kumar <rakeshkumar1146@yahoo.com>
2025-01-21 15:44:03 +0530

    Playing with JSX syntax and Component Composition

--- creating-app/src/App.jsx
+++ creating-app/src/App.jsx
@@ -1,3 +1,25 @@
-const App = () => <h1>React!!!</h1>
+const greeting = <span>Hey React::</span>;
+
+const title = (
+  <h1>
+    {greeting}
+    The Title
+  </h1>
+);
+
+const SubTitle = () => <h2>The SubTitle</h2>;
+
+const App = () => (
+  <div>
+
+    {title}
+    <SubTitle />
+    {SubTitle()}
+    
+    <div>Something in div. Caculating 100 + 400 = {100 + 400}</div>
+  </div>
+);
 
 export default App;
+
+
